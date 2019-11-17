cedad077184494fd16260e0b73c9c50e63bd9936
Fabricio Catae <fcatae@hotmail.com>
2019-11-16 22:47:26 -0300

    working

--- smart-connector/src/test/java/BasicTestCase.java
+++ smart-connector/src/test/java/BasicTestCase.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
+import org.mule.runtime.api.event.Event;
 
 public class BasicTestCase extends MuleArtifactFunctionalTestCase {
 
@@ -9,7 +10,8 @@ public class BasicTestCase extends MuleArtifactFunctionalTestCase {
 	}
 	
 	@Test
-	public void validate() {
-		
+	public void validate() throws Exception {
+		Event event = flowRunner("testFlow").run();
+		String result = event.getMessage().getPayload().getValue().toString();
 	}
 }
